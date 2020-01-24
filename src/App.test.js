8be7f4c62ed08b8535eb103f0fb0b0b0f29e71ae
Deonte Cooper <deonte.cooper12@gmail.com>
2020-01-24 02:26:55 -0700

    Update test

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,7 @@ import App from "./App";
 describe("App", () => {
   it("should render without throwing an error", () => {
     const wrapper = shallow(<App />);
-    expect(wrapper.contains(<div className="My-Site" />));
+    expect(wrapper.contains(<main className="My-Site" />));
   });
 
   it("should contain a header", () => {
