a76dbe71628463e9ea2fca2aa7ff48df7e566b51
Deonte Cooper <deonte.cooper12@gmail.com>
2020-01-28 13:30:52 -0700

    Updates

---
+++ precache-manifest.58735b578019dcbddfc6970c2ac0ee61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e6242eb14eac3d8f8d115124f6d2651",
+    "url": "/deontecoopersite/index.html"
+  },
+  {
+    "revision": "483dc6857d21eba203a4",
+    "url": "/deontecoopersite/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "445a14c7525e0f62ad41",
+    "url": "/deontecoopersite/static/js/2.318e0177.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/deontecoopersite/static/js/2.318e0177.chunk.js.LICENSE"
+  },
+  {
+    "revision": "483dc6857d21eba203a4",
+    "url": "/deontecoopersite/static/js/main.3bf6fe96.chunk.js"
+  },
+  {
+    "revision": "22214382e1e709c6f115",
+    "url": "/deontecoopersite/static/js/runtime-main.21f10e3d.js"
+  }
+]);
