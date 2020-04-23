8b79a00075fd4f240f57662b0c120bed86ab2148
Deonte Cooper <deonte.cooper12@gmail.com>
2020-04-23 12:58:31 -0600

    Add Bio path to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Header from "./components/Header/Header";
+import Bio from "./components/Bio/Bio";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <main className="My-Site">
       <Header />
       <Route exact path="/" component={Home} />
+      <Route exact path="/Bio" component={Bio} />
     </main>
   );
 }
