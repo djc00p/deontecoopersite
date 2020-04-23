9cb9bf5378ff081d35b8f1f9aa94c28cb044638e
Deonte Cooper <deonte.cooper12@gmail.com>
2020-04-23 13:24:37 -0600

    Update App.js with Project Route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Header from "./components/Header/Header";
 import Bio from "./components/Bio/Bio";
+import Projects from "./components/Projects/Projects";
 import "./App.css";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Route exact path="/" component={Home} />
       <Route exact path="/Bio" component={Bio} />
+      <Route exact path="/Projects" component={Projects} />
     </main>
   );
 }
