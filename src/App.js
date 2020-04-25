9b9a30ec8533d1122a6e1e6b1a3ef3934aab9309
Deonte Cooper <deonte.cooper12@gmail.com>
2020-04-25 16:30:05 -0600

    Add Apollo Client Setup for frontend

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route } from "react-router-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+// components
 import Home from "./components/Home/Home";
 import Header from "./components/Header/Header";
 import Bio from "./components/Bio/Bio";
 import Projects from "./components/Projects/Projects";
+
+// CSS
 import "./App.css";
+
+// apollo client setup
+const client = ApolloClient({
+  uri: "https://api.github.com/graphql"
+});
 
 function App() {
   return (
-    <main className="My-Site">
-      <Header />
-      <Route exact path="/" component={Home} />
-      <Route exact path="/Bio" component={Bio} />
-      <Route exact path="/Projects" component={Projects} />
-    </main>
+    <ApolloProvider client={client}>
+      <main className="My-Site">
+        <Header />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/Bio" component={Bio} />
+        <Route exact path="/Projects" component={Projects} />
+      </main>
+    </ApolloProvider>
   );
 }
 
