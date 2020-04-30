b7abf2a5efc8e5d67f0a3d896cd13f67ba7ea0ca
Deonte Cooper <deonte.cooper12@gmail.com>
2020-04-30 11:46:40 -0600

    Comment out the contact form until i can get it full functioning and replaceing with contact info temp

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -39,45 +39,56 @@ class Contact extends Component {
   render() {
     return (
       <div className="form-container">
-        <form
-          id="contact-form"
-          onSubmit={this.handleSubmit.bind(this)}
-          method="POST"
-          className="form"
-        >
-          <div className="form-group">
-            <label htmlFor="name">Name</label>
-            <input
-              type="text"
-              className="form-control"
-              value={this.state.name}
-              onChange={this.onNameChange.bind(this)}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="EmailInput">Email address</label>
-            <input
-              type="email"
-              className="form-control"
-              aria-describedby="emailHelp"
-              value={this.state.email}
-              onChange={this.onEmailChange.bind(this)}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="message">Message</label>
-            <textarea
-              className="form-control"
-              rows="5"
-              value={this.state.message}
-              onChange={this.onMessageChange.bind(this)}
-            ></textarea>
-          </div>
-          <button type="submit" className="sub">
-            Submit
-          </button>
-        </form>
+        <h1>Contact me</h1>
+        <p className="form-control">Email: deonte.cooper12 at gmail.com</p>
+        <p className="form-control">
+          LinkedIn: https://www.linkedin.com/in/deonte-cooper/
+        </p>
+        <p className="form-control">Twitter: https://twitter.com/DJC00P23</p>
       </div>
+
+      // Contact Form Not fully functioning yet
+
+      // <div className="form-container">
+      //   <form
+      //     id="contact-form"
+      //     onSubmit={this.handleSubmit.bind(this)}
+      //     method="POST"
+      //     className="form"
+      //   >
+      //     <div className="form-group">
+      //       <label htmlFor="name">Name</label>
+      //       <input
+      //         type="text"
+      //         className="form-control"
+      //         value={this.state.name}
+      //         onChange={this.onNameChange.bind(this)}
+      //       />
+      //     </div>
+      //     <div className="form-group">
+      //       <label htmlFor="EmailInput">Email address</label>
+      //       <input
+      //         type="email"
+      //         className="form-control"
+      //         aria-describedby="emailHelp"
+      //         value={this.state.email}
+      //         onChange={this.onEmailChange.bind(this)}
+      //       />
+      //     </div>
+      //     <div className="form-group">
+      //       <label htmlFor="message">Message</label>
+      //       <textarea
+      //         className="form-control"
+      //         rows="5"
+      //         value={this.state.message}
+      //         onChange={this.onMessageChange.bind(this)}
+      //       ></textarea>
+      //     </div>
+      //     <button type="submit" className="sub">
+      //       Submit
+      //     </button>
+      //   </form>
+      // </div>
     );
   }
 
