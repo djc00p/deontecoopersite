bc2396d9e5dede4635f79f80bb071362892cab4e
Deonte Cooper <deonte.cooper12@gmail.com>
2020-05-01 16:40:33 -0600

    Add SoundCloud test and Snapshot, Update projects test and snapshot

--- src/components/Projects/Projects.test.js
+++ src/components/Projects/Projects.test.js
@@ -3,8 +3,8 @@ import { shallow, mount, render } from "enzyme";
 import Projects from "./Projects";
 
 describe("Projects", () => {
-  it("should render withour throwing an error", () => {
+  it("should create snapshot of Projects", () => {
     const wrapper = shallow(<Projects />);
-    expect(wrapper.contains(<div className="home-page" />));
+    expect(wrapper).toMatchSnapshot();
   });
 });
