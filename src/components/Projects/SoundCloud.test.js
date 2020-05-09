2cc8f873825ceeda63ab8e6839ac6ef3a04ea2bf
Deonte Cooper <deonte.cooper12@gmail.com>
2020-05-09 10:23:52 -0600

    Move SoundCloud to own component folder

--- src/components/Projects/SoundCloud.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { shallow, mount, render } from "enzyme";
-import SoundCloud from "./SoundCloud";
-
-it("should create snapshot of SoundCloud", () => {
-  const wrapper = shallow(<SoundCloud />);
-  expect(wrapper).toMatchSnapshot();
-});
