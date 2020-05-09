7eaefcb1b3e2e7d3ac33a07b10f8733d3b191ee7
Deonte Cooper <deonte.cooper12@gmail.com>
2020-05-09 10:37:04 -0600

    Add Browser Router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 const router = (
-  <HashRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
 
 ReactDOM.render(router, document.getElementById("root"));
